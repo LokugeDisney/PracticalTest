c3c00b54546780e2258b63f1e294a12042acd45a
001192985 <Disney.LOKUGE@student.tafesa.edu.au>
2024-03-23 01:38:28 +1030

    Change the ticket price 2nd round

--- Ticketing/TicketPrice.cs
+++ Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 37.5m;
-        const decimal mdecGeneral = 28.75m;
-        const decimal mdecBox = 64.0m;
+        const decimal mdecBalcony = 38.5m;
+        const decimal mdecGeneral = 29.75m;
+        const decimal mdecBox = 65.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
